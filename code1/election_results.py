c5b9d7d798e9b2eb4c52046703f33f87b86b4a17
Serdar Tumgoren <zstumgoren@gmail.com>
2014-01-25 15:12:43 -0800

    stub files for first two phases of code

---
+++ code1/election_results.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+A monstrosity of an election results script. Bundles together waaaay too much 
+functionality in a single module.
+"""
+# Download CSV from GDocs
+
+# Parse CSV
+
+# Data processing
+
+# Parse names
+
+# Standardize party abbreviations
+
+# Determine winner, if any
+
+# Calculate total votes for race
+# Calculate margin of victory
+
+# Generate cleaned up spreadsheet of results
+# To each row, add total votes, margin of victory, winner flag
---
+++ code2/election_results.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+In this second pass at our script, we'll start refactoring our code to use functions.
+"""
+# Download CSV from GDocs
+
+# Parse CSV
+
+# Data processing
+
+# Parse names
+
+# Standardize party abbreviations
+
+# Determine winner, if any
+
+# Calculate total votes for race
+# Calculate margin of victory
+
+# Generate cleaned up spreadsheet of results
+# To each row, add total votes, margin of victory, winner flag
