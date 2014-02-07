ed491e2e7a22a3b630604eca8e531653707242bc
Serdar Tumgoren <zstumgoren@gmail.com>
2014-02-06 19:37:38 -0800

    Add tests for Candidate.party

--- elex4/tests/test_models.py
+++ elex4/tests/test_models.py
@@ -6,6 +6,18 @@ class TestCandidate(TestCase):
 
     def test_candidate_name(self):
         "Candidates should have first_name and last_name attributes"
-        cand = Candidate("Smith, Joe")
+        cand = Candidate("Smith, Joe", "GOP")
         self.assertEquals(cand.first_name, "Joe")
         self.assertEquals(cand.last_name, "Smith")
+
+    def test_party_gop(self):
+        cand = Candidate("Smith, Joe", "GOP")
+        self.assertEquals(cand.party, 'REP')
+
+    def test_clean_party_dem(self):
+        cand = Candidate("Smith, Joe", "Democratic")
+        self.assertEquals(cand.party, 'DEM')
+
+    def test_clean_party_others(self):
+        cand = Candidate("Smith, Joe", "Green")
+        self.assertEquals(cand.party, 'GREEN')
