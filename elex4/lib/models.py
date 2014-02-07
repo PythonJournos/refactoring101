e9f013c2f1383d8c5c4c60780d59f92e16758083
Serdar Tumgoren <zstumgoren@gmail.com>
2014-02-06 19:51:25 -0800

    add party attribute to Candidate

--- elex4/lib/models.py
+++ elex4/lib/models.py
@@ -1,5 +1,17 @@
 
 class Candidate(object):
 
-    def __init__(self, raw_name):
+    def __init__(self, raw_name, party):
         self.last_name, self.first_name = [name.strip() for name in raw_name.split(",")]
+        self.party = self.__clean_party(party)
+
+    # Private methods
+    def __clean_party(self, party):
+        party = party.strip().upper()
+        if party.startswith('GOP'):
+            party_clean = 'REP'
+        elif party.startswith('DEM'):
+            party_clean = 'DEM'
+        else:
+            party_clean = party
+        return party_clean
