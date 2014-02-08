29076466953ac18b0dba054d69e02608fca92910
Serdar Tumgoren <zstumgoren@gmail.com>
2014-02-07 17:19:56 -0800

    Add Candidate county result access

--- elex4/lib/models.py
+++ elex4/lib/models.py
@@ -4,9 +4,11 @@ class Candidate(object):
     def __init__(self, raw_name, party):
         self.last_name, self.first_name = [name.strip() for name in raw_name.split(",")]
         self.party = self.__clean_party(party)
+        self.county_results = {}
         self.votes = 0
 
     def add_votes(self, county, votes):
+        self.county_results[county] = votes
         self.votes += votes
 
     # Private methods
