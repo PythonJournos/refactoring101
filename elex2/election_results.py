ba218c99d309f850b5e9c792f9a7415d5bd09346
Serdar Tumgoren <zstumgoren@gmail.com>
2014-02-13 23:35:42 -0800

    add Q and A for ifname

--- elex2/election_results.py
+++ elex2/election_results.py
@@ -157,6 +157,8 @@ def write_csv(summary):
                 writer.writerow(results)
 
 
-
+# Q: What on earth is this __name__ == __main__ thing?
+# A: Syntax that let's you execute a module as a script.
+# http://docs.python.org/2/tutorial/modules.html#executing-modules-as-scripts
 if __name__ == '__main__':
     main()
